d9f1cf1ebb4b51b6b541bfcb81afa426378ab55e
elibillinger <eebillin@hotmail.com>
2021-10-12 22:48:33 -0600

    Moved script in html file to js file and connected them

--- game.js
+++ game.js
@@ -5,6 +5,21 @@ var click1 = "";
 var click2 = "";
 var matched = 0;
 
+const canvas = document.getElementById("gameBoard")
+const ctx = canvas.getContext("2d")
+const cu = new Image()
+const rover = new Image()
+cu.src = "./cardImages/boulder.jpg"
+rover.src = "./cardImages/rover.jpg"
+cu.onload = () => {
+    ctx.drawImage(cu, 0, 0, 100, 100)
+    ctx.drawImage(cu, 100, 100, 100, 100)
+}
+rover.onload = () => {
+    ctx.drawImage(rover, 100, 0, 100, 100)
+    ctx.drawImage(rover, 0, 100, 100, 100)
+}
+
 function checkClicks(){
     if(hasClicked == 2){ //check if user has clicked two cards
         hasClicked = 0; //reset click counter to zero
